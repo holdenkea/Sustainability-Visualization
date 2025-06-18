2e8c4925b0175e8fd9d4ae3a39fd5a7ec835a25a
holdenkea <holdenkea@gmail.com>
2025-06-17 18:28:08 -0700

    timeframe slider logic added

--- data/constants.py
+++ data/constants.py
@@ -1,16 +1,40 @@
 from data.csv_source import df
+import pandas as pd
 
-# location and unit lists
+##################################################################################################
+# location options and defaults for filters section
 location_options = []
-unit_options = []
 
-# gets the unique locations and units from the dataframe and sorts them alphabetically
 for location in sorted(df['location'].unique()):
     location_options.append({'label': location, 'value': location})
+
+default_locations = df['location'].unique().tolist()
+
+##################################################################################################
+# unit options and defaults for filters section
+unit_options = []
 
 for unit in sorted(df['unit'].unique()):
     unit_options.append({'label': unit, 'value': unit})
 
-# gets unique locations and units from dataframe and turns them into a list for the checklist
-default_locations = df['location'].unique().tolist()
 default_units = ['kWh']
+
+##################################################################################################
+# timestamp options, marks (ticks), and options for filters section
+timestamp_options = []
+
+# convert to datetime
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+# take out only date
+df['date_only'] = df['timestamp'].dt.date
+
+# get unique dates
+timestamp_options = sorted(df['date_only'].unique())
+
+timestamp_marks = {}
+for index, timestamp in enumerate(timestamp_options):
+    timestamp_marks[index] = timestamp.strftime('%Y-%m-%d')
+
+default_min_timestamp = 0
+default_max_timestamp = len(timestamp_options) - 1
--- layouts/homepage/filters.py
+++ layouts/homepage/filters.py
@@ -1,5 +1,7 @@
 from dash import html, dcc
-from data.constants import location_options, default_locations, unit_options, default_units
+from data.constants import (location_options, default_locations, 
+                            unit_options, default_units, 
+                            timestamp_options, default_min_timestamp, default_max_timestamp, timestamp_marks)
 
 # filters section
 filters = html.Div([
@@ -20,6 +22,20 @@ filters = html.Div([
             id='unit-filter',
             options=unit_options,
             value=default_units
+        )
+    ]),
+
+    html.Div([
+        html.Label("Timeframe"),
+        dcc.RangeSlider(
+            id='timestamp-filter',
+            min=0,
+            max=len(timestamp_options) - 1,
+            value=[default_min_timestamp, default_max_timestamp],
+            marks=timestamp_marks,
+            allowCross=False,
+            step=None,
+            tooltip={"placement": "bottom", "always_visible": False}
         )
     ])
 ])
