33bc75f558e67c2941600c18324a28010a1bce2e
holdenkea <holdenkea@gmail.com>
2025-06-23 23:19:20 -0700

    choropleth map implemented, building coords added to geojson file and works with filter sidebar

--- callbacks/graph_callbacks.py
+++ callbacks/graph_callbacks.py
@@ -2,28 +2,32 @@ from dash import Input, Output, callback
 import plotly.express as px
 from data.csv_source import df
 from data.constants import timestamp_options
-
 import plotly.graph_objects as go
 
 import json
 
-# load building coordinates
-with open('assets/buildings.json', 'r') as file:
-    building_coords = json.load(file)
-
 # function to truncate names
 def truncate_name(name):
-    if len(name) <= 9:
-        return name
-    return name[:5] + '...'
-
-# function to convert pixels to Plotly 0-100 scale (imgs origin is top left, plotly origin is bottom left)
-def normalize_pixels(x, y, img_w=791, img_h=791):
-    x_norm = (x / img_w) * 100
-    y_norm = (1 - (y / img_h)) * 100
-
-    return x_norm, y_norm
+    if "Green Music Center".lower() in name.lower():
+        return "Green Music Center"
+    elif "Ives Hall".lower() in name.lower():
+        return "Ives Hall"
+    elif "Nichols Hall".lower() in name.lower():
+        return "Nichols Hall"
+    elif "Physical Education".lower() in name.lower():
+        return "Physical Education"
+    elif "Rachel Carson Hall".lower() in name.lower():
+        return "Rachel Carson Hall"
+    elif "Student Health Center".lower() in name.lower():
+        return "Student Health Center"
+    elif "Student Center".lower() in name.lower():
+        return "Student Center"
+    elif "Wine Spectator Learning".lower() in name.lower():
+        return "Wine Spectator Center"
+    else:
+        return None
  
+# callback which updates the graphs when any of the input ids are changed 
 @callback(
     Output('graph1', 'figure'),
     Output('graph2', 'figure'),
@@ -53,35 +57,75 @@ def update_graphs(selected_locations, selected_units, selected_timestamps):
     # truncate location name for display
     aggregated_df['truncated_location'] = aggregated_df['location'].apply(truncate_name)
 
-    # plotly graph object for graph1 (heatmap)
-    figure1 = go.Figure()
+    # code for debugging contents of aggregated_df
+    aggregated_df.to_csv('temp_agg_df.csv', index=False)
 
-    # campus map
-    figure1.update_layout(
-        images=[dict(
-            source='/assets/ssu_map_cropped.png',
-            xref='x',
-            yref='y',
-            x=0,
-            y=100,
-            sizex=100,
-            sizey=100,
-            sizing='stretch',
-            layer='below',
-        )],
-        xaxis=dict(visible=False, range=[0, 100]),
-        yaxis=dict(visible=False, range=[0, 100]),
-        title='Map of Energy Used by Location'
+    # update map (plotly graph objects with MapLibre map)
+    figure1 = update_figure1(aggregated_df)
+
+    # update plotly bar graph 
+    figure2 = update_figure2(aggregated_df)
+
+    # update plotly donut chart 
+    figure3 = update_figure3(aggregated_df)
+
+    return figure1, figure2, figure3
+
+def update_figure1(aggregated_df):
+
+    # coordinates defining campus shape
+    campus_polygon = [
+        [38.343309, -122.679879],
+        [38.343311, -122.674769],
+        [38.346074, -122.672685],
+        [38.346182, -122.666775],
+        [38.336137, -122.666731],
+        [38.336032, -122.679787],
+        [38.343309, -122.679879]
+    ]
+
+    # campus scattermap trace
+    campus_map_trace = go.Scattermap(
+        lat=[pt[0] for pt in campus_polygon],
+        lon=[pt[1] for pt in campus_polygon],
+        mode='lines',
+        fill='toself',
+        fillcolor='rgba(0, 100, 255, 0.1)',
+        line=dict(color='blue'),
+        hovertext='Sonoma State University',
+        hoverinfo='text'
     )
 
-    # convert coordinates to scatter traces (shapes)
-    building_traces = coords_to_scatter_traces(building_coords)
+    # load building GeoJSON file
+    with open('assets/geo-json-buildings.json', 'r') as file:
+        geo_json_buildings = json.load(file)
 
-    # add each building to figure1
-    for trace in building_traces:
-        figure1.add_trace(trace)
+    # campus choropleth map trace
+    choropleth_map_trace = go.Choroplethmap(
+        geojson=geo_json_buildings,
+        locations=aggregated_df['truncated_location'],
+        z=aggregated_df['value'],
+        featureidkey="properties.id",
+        colorscale="Jet",
+        marker_opacity=0.7,
+    )
 
-    # plotly bar graph for graph2
+    #figure1 = go.Figure(campus_map_trace)
+    figure1 = go.Figure(data=[choropleth_map_trace, campus_map_trace])
+
+    figure1.update_layout(
+        map=dict(
+            style='satellite-streets',  
+            center=dict(lat=38.340931449672006, lon=-122.67307511912622),  
+            zoom=15
+        ),
+        margin=dict(l=0, r=0, t=0, b=0),
+    )
+
+    return figure1
+
+def update_figure2(aggregated_df):
+
     figure2 = px.bar(
         aggregated_df,
         x='value',
@@ -92,50 +136,21 @@ def update_graphs(selected_locations, selected_units, selected_timestamps):
         hover_name='location'    
     )
 
-    # plotly donut chart for graph3
+    return figure2
+
+def update_figure3(aggregated_df):
+
     figure3 = px.pie(
-        aggregated_df,
-        names='truncated_location',
-        values='value',
-        title='Percent of Energy Used by Location',
-        hole=0.5,
-        hover_name='location'
-    )
+            aggregated_df,
+            names='truncated_location',
+            values='value',
+            title='Percent of Energy Used by Location',
+            hole=0.5,
+            hover_name='location'
+        )
 
     # display percentage values and labels
     figure3.update_traces(textinfo='percent+label', rotation=270)
 
-    return figure1, figure2, figure3
- 
-# function to convert building coordinates (json) to scatter traces (shapes with hover visuals)
-def coords_to_scatter_traces(building_coords):
-
-    building_traces = []
-
-    for building in building_coords:
-        name = building['name']
-
-        # unpack coords (*), and pass to normalize pixels
-        (x1, y1) = normalize_pixels(*building['top_left'])
-        (x2, y2) = normalize_pixels(*building['bottom_right'])
-
-        # rectange coords
-        x_coords = [x1, x2, x2, x1, x1]
-        y_coords = [y1, y1, y2, y2, y1]
-
-        building_trace = go.Scatter(
-        x=x_coords,
-        y=y_coords,
-        fill='toself',
-        fillcolor='rgba(0, 0, 255, 0.25)',
-        line=dict(color='blue', width=0),
-        hoverinfo='text',
-        text=name,
-        mode='lines',
-        name=name,
-        )
-        building_traces.append(building_trace)
-        
-
-    return building_traces
+    return figure3
 
